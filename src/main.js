7dababa11880bbf41e0c5d5fa912276cf29d30ff
Denise Barg <den.barg.23@lehre.mosbach.dhbw.de>
2024-11-03 20:29:18 +0100

    Added: Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import './bootstrap/dist/css/bootstrap.min.css';
-import './bootstrap/dist/js/bootstrap.min.js';
-
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 createApp(App).mount('#app')
