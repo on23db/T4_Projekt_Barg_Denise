6f723e334c7d59cf9c3c40f6317e77966ea68448
Denise Barg <den.barg.23@lehre.mosbach.dhbw.de>
2024-11-05 11:46:55 +0100

    Deleted: Default Vue "Hello World" Components for clean up

--- backend/index.php
+++ backend/index.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root"; // Standardbenutzername in XAMPP
 $password = "";     // Standardpasswort ist leer
-$dbname = "database"; // Setze hier den Namen deiner Datenbank ein
+$dbname = "database"; // Name der Datenbank
 
 // Verbindung zur Datenbank herstellen
 $conn = new mysqli($servername, $username, $password, $dbname);
